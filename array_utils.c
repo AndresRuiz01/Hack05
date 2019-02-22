96d7bf67a2cd0ae00cc027ae729c46ccd7ec9fc5
Andres Ruiz <andres@Andress-MacBook-Air.local>
2019-02-21 18:01:45 -0600

    Update array_utils.c

--- array_utils.c
+++ array_utils.c
@@ -1,3 +1,13 @@
+/*
+ * File: array_utils.c
+ * Author: Andres Ruiz and Teresa Gramer
+ * Date: 2019/02/22
+ *
+ * A program that creates the functions containsWithin
+ * contains, reverse, and orderStatistic
+ *
+ */
+
 #include "array_utils.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -48,89 +58,4 @@ int orderStatistic(int *arr, int size, int i){
   return element;
 }
 
-  // TESTING //
-  // TESTING //
-
-int main(){
-
-  int result;
-  int numPassed = 0;
-  int numFailed = 0;
-
-  int arr[5] = {1,2,3,4,5};
-  result = contains(arr, 5, 5);
-  if(result != 1) {
-    printf("FAILED: Array does contain 5\n");
-    numFailed++;
-   }
-  else {
-    printf("PASSED\n");
-    numPassed++;
-   }
-
-  int arr2[5] = {2,4,6,8,10};
-  result = contains(arr2, 5, 5);
-  if(result != 0) {
-    printf("FAILED: Array does not contain 5\n");
-    numFailed++;
-   }
-  else {
-    printf("PASSED\n");
-    numPassed++;
-   }
-
-  int arr3[10] = {1,2,3,4,5,6,7,8,9,10};
-  result = containsWithin(arr3, 10, 7, 3, 8);
-  if(result != 1) {
-    printf("FAILED: Array range does contain 7\n");
-    numFailed++;
-    }
-   else {
-      printf("PASSED\n");
-      numPassed++;
-    }
-
-    int arr4[10] = {1,2,3,4,5,6,7,8,9,10};
-    result = containsWithin(arr4, 10, 8, 1, 5);
-    if(result != 0) {
-       printf("FAILED: Array range does not contain 8\n");
-       numFailed++;
-     }
-    else {
-       printf("PASSED\n");
-       numPassed++;
-     }
-
-    int arr5[10] = {10,9,8,7,6,5,4,3,2,1};
-    result = orderStatistic(arr5, 10, 3);
-    if(result != 3) {
-      printf("FAILED: Array range does not contain 8\n");
-      numFailed++;
-    }
-    else {
-      printf("PASSED\n");
-      numPassed++;
-    }
-
-    int arr6[10] = {2,4,6,8,10,1,3,5,7,9};
-    result = orderStatistic(arr6, 10, 7);
-    if(result != 7) {
-      printf("FAILED: Array range does not contain 8\n");
-      numFailed++;
-    }
-    else {
-      printf("PASSED\n");
-      numPassed++;
-    }
-
-    int arr7[10] = {1,2,3,4,5,6,7,8,9,10};
-    reverse(arr7, 10);
-
-
-
-  printf("Number Test Cases Passed: %6d\n", numPassed);
-  printf("Number Test Cases Failed: %6d\n", numFailed);
-  printf("Percent Passed:           %6.2f\n", 100.0 * numPassed / (numPassed + numFailed));
-
-  return 0;
-}
+int main(){}
